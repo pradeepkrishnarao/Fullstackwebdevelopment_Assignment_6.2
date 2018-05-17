e2bde7faaaded84906a19577bcd49075ead4e6e2
pradeepkrishnarao <36490187+pradeepkrishnarao@users.noreply.github.com>
2018-05-17 09:09:05 -0400

    Add files via upload

---
+++ assignment6.2.js
@@ -0,0 +1,22 @@
+//hold ctrl+shift+J shortcut key while google chrome/opera/firefox/microsoft edge/ page is opened to open the console window and input the //following statements and press the enter key to see the output within the console window
+var recipeName = "Creamy squash linguine"; //string
+var preparationTime=45; //integer
+var recipeBy = "Jimmy Cutter"; //string
+var isVegetarian = true; //boolean
+var calories = 223.7; //decimal
+console.log("Recipe details:"); 
+console.log("-------------");
+console.log("Recipe Name:"+recipeName);
+console.log("Preparation Time:"+preparationTime);
+console.log("Recipe By:"+recipeBy);
+console.log("is Vegetarian Dish:"+isVegetarian);
+console.log("Calories:"+calories);
+preparationTime=60; //preparationTime is initialized to a new integer value
+recipeBy="Jimmy Cutter George" //recipeBy is initialized to a new string value
+console.log("Recipe details after update:"); //label
+console.log("--------------");
+console.log("Recipe Name:"+recipeName); //Using string concatenation label and value is printed together
+console.log("Preparation Time:"+preparationTime);
+console.log("RecipeBy:"+recipeBy);
+console.log("is Vegetarian Dish:"+isVegetarian);
+console.log("Calories:"+calories);
